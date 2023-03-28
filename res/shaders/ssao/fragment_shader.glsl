a9d5aba3e12e5536c3b833d3227edf6ceaf72170
Fahd <fahd31256@gmail.com>
2023-03-28 16:45:31 +0100

    Tweak SSAO parameters

--- res/shaders/ssao/fragment_shader.glsl
+++ res/shaders/ssao/fragment_shader.glsl
@@ -12,8 +12,8 @@ uniform vec3 samples[128];
 
 // parameters (you'd probably want to use them as uniforms to more easily tweak the effect)
 int kernelSize = 128;
-float radius = 0.5;
-float bias = 0.025;
+float radius = 0.25;
+float bias = 0.012;
 
 // tile noise texture over screen based on screen dimensions divided by noise size
 const vec2 noiseScale = vec2(800.0/4.0, 600.0/4.0); 
@@ -53,5 +53,5 @@ void main()
     }
     occlusion = 1.0 - (occlusion / kernelSize);
     
-    FragColor = occlusion;
+    FragColor = pow(occlusion, 10.0);
 }
